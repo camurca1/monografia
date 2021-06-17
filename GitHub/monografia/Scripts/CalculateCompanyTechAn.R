49f53a706420f16261d6b2f6c54e98515478793d
camurca1 <alexandre.camurca@unesp.br>
2021-06-16 21:32:49 -0300

    Implementado cálculos para decidir Compra e Venda da ação na análise técnica

--- GitHub/monografia/Scripts/CalculateCompanyTechAn.R
+++ GitHub/monografia/Scripts/CalculateCompanyTechAn.R
@@ -31,30 +31,64 @@ calc_est <- function(x){
   return(x)
 }
 
+calc_decisaoCV <- function(x){
+  x$mov.MM <- if_else(x$MMS.rapida > x$MME.lenta, 1,
+                          if_else(x$MMS.rapida < x$MME.lenta, -1, 0, missing = 0),
+                          missing = 0)
+  x$mov.MACD <- if_else(x$MACD1 > x$MACD2, 1, -1, missing = 0)
+  x$mov.est <- if_else(x$estK > 0.8 & x$estD > 0.8,
+                           -1,
+                           if_else(x$estK < 0.2 & x$estD < 0.2,
+                                   1,
+                                   0,
+                                   missing = 0),
+                           missing = 0)
+  x$aux.sinal.MM <- roll_sum(x$mov.MM, n = 3, align = "right", fill = 0)
+  x$aux.sinal.MACD <- roll_sum(x$mov.MACD, n = 3, align = "right", fill = 0)
+  x$sinal.MM <- if_else(x$aux.sinal.MM == 3,
+                            1,
+                            if_else(x$aux.sinal.MM == -3,
+                                    1,
+                                    0,
+                                    missing = 0),
+                            missing = 0)
+  x$sinal.MACD <- if_else(x$aux.sinal.MACD == 3,
+                              1,
+                              if_else(x$aux.sinal.MACD == -3,
+                                      -1,
+                                      0,
+                                      missing = 0),
+                              missing = 0)
+  x$aux.sinal.decisao <- x$sinal.MACD + x$sinal.MM + x$mov.est
+  x$sinal.decisao <- if_else(x$aux.sinal.decisao > 1,
+                                 1,
+                                 if_else(x$aux.sinal.decisao < -1,
+                                         -1,
+                                         0,
+                                         missing = 0),
+                                 missing = 0)
+  x$decisao.final <- if_else(x$sinal.decisao == 1,
+                                 "COMPRA",
+                                 if_else(x$sinal.decisao == -1,
+                                         "VENDA",
+                                         "NEUTRO",
+                                         missing = "NEUTRO"),
+                                 missing = "NEUTRO")
+  x[ ,5:20] <- NULL
+  return(x)
+}
+
 precos.empresas <- readRDS("Data/precos_acoes")
 precos.analise <- readRDS("Data/retorno_volatilidade_acoes")
+precos.analise$retornos <- NULL
+precos.analise$volatilidade <- NULL
 
 l.precos <- split(precos.analise, precos.analise$ticker)
 l.precos <- lapply(l.precos, calc_MME)
 l.precos <- lapply(l.precos, calc_MACD)
 l.precos <- lapply(l.precos, calc_est)
+l.precos <- lapply(l.precos, calc_decisaoCV)
 precos.analise <- unsplit(l.precos, precos.analise$ticker)
-
-ELET3 <- subset(precos.analise, precos.analise$ticker == "ELET3")
-ELET3$mov.MM <- if_else(ELET3$MMS.rapida > ELET3$MME.lenta, 1,
-                         if_else(ELET3$MMS.rapida < ELET3$MME.lenta, -1, 0, missing = 0),
-                         missing = 0)
-ELET3$mov.MACD <- if_else(ELET3$MACD1 > ELET3$MACD2, 1, -1, missing = 0)
-ELET3$mov.est <- if_else(ELET3$estK > 0.8 & ELET3$estD > 0.8,
-                         -1,
-                         if_else(ELET3$estK < 0.2 & ELET3$estD < 0.2,
-                                 1,
-                                 0,
-                                 missing = 0),
-                         missing = 0)
-
-plot(ELET3$ref.date, ELET3$price.adjusted, type = "l")
-plot(ELET3$ref.date, ELET3$mov.MME, type = "l")
 
 saveRDS(precos.analise, "Data/indAT")
 
