aa57289b0887d64a6d1b6968570b0f1324e066e7
camurca1 <alexandre.camurca@unesp.br>
2021-08-10 17:50:30 -0300

    Melhorias de performance e documentação.

--- GitHub/monografia/Scripts/GetAllCompanyPrices.R
+++ GitHub/monografia/Scripts/GetAllCompanyPrices.R
@@ -1,41 +1,64 @@
+# Escrito por: Alexandre Camurça Silva de Souza
+# Ambiente RStudio Desktop 1.4.1717 "Juliet Rose"
+
+# Etapa 2 - Download das cotações históricas
+
+
+# limpar memória e desativar notação científica
 rm(list = ls())
 options(scipen = 999)
 
+#### Gereciamento de pacotes ####
 
-library(BatchGetSymbols)
-library(lubridate)
+# informar os pacotes que serao utilizados no script
+pacotes <- c("BatchGetSymbols", "lubridate")
+
+# instalar pacotes ausentes
+pacotes_instalados <- pacotes %in% rownames(installed.packages())
+if (any(pacotes_instalados == FALSE)) {
+  install.packages(c(pacotes[!pacotes_instalados]))
+}
+
+# carregar pacotes
+invisible(lapply(pacotes, library, character.only = TRUE))
+
+
+#### Importar dados financeiros das empresas ####
 
 cia.info <- readRDS("Data/cia_info_reduzido")
 tickers <- readRDS("Data/tabelatickers")
 
-#### Importar dados financeiros ####
-
-# Definir parametros
+# Definir parâmetros do download
 papel <- paste0(tickers$simbolo, ".SA")
 dt_inicial <- ymd("2014-01-01")
 dt_final <- ymd("2020-12-31")
-thresh.bad.data <- 0.95   # filtro de qualidade de dados
-bench.ticker <- '^BVSP'   # ibov como benchmark
-cache.folder <- 'Data/BGS_Cache' # cache de memoria
+thresh.bad.data <- 0.95
+bench.ticker <- "^BVSP"
+cache.folder <- "Data/BGS_Cache"
 
-# importar dados financeiros
-ls_papel <- BatchGetSymbols(papel, dt_inicial, dt_final, thresh.bad.data, bench.ticker)
+# importar cotações das ações
+ls_papel <- BatchGetSymbols(papel, dt_inicial, dt_final,
+                            thresh.bad.data, bench.ticker)
 df_papel <- ls_papel$df.tickers
 df_papel$ticker <- gsub("\\.SA", "", df_papel$ticker)
 df_controle <- ls_papel$df.control
 df_controle$ticker <- gsub("\\.SA", "", df_controle$ticker)
 
-# importar benchmark
-ls_ibov <- BatchGetSymbols("^BVSP", dt_inicial, dt_final, thresh.bad.data, bench.ticker)
+# importar cotações do ibov
+ls_ibov <- BatchGetSymbols("^BVSP", dt_inicial, dt_final,
+                           thresh.bad.data, bench.ticker)
 df_ibov <- ls_ibov$df.tickers
 df_ibov$ticker <- gsub("\\.SA", "", df_ibov$ticker)
 
 # tratamento tabela tickers
 tickers <- left_join(tickers,
-                    select(df_controle,
-                           ticker,
-                           threshold.decision),
-                    by = c("simbolo" = "ticker"))
+  select(
+    df_controle,
+    ticker,
+    threshold.decision
+  ),
+  by = c("simbolo" = "ticker")
+)
 tickers <- tickers %>%
   na.omit(threshold.decision) %>%
   filter(threshold.decision != "OUT")
@@ -48,22 +71,24 @@ saveRDS(tickers, "Data/tabelatickers")
 saveRDS(df_ibov, "Data/precosibov")
 rm(df_controle, df_papel, df_ibov)
 
-# selecionar acoes mais negociadas
+# selecionar tickers mais negociados de cada empresa
 preco.papel <- readRDS("Data/precos_acoes")
 preco.papel.agregado <- aggregate(preco.papel$volume,
-                                  by = list(Tickers=preco.papel$ticker),
-                                  FUN = mean)
+  by = list(Tickers = preco.papel$ticker),
+  FUN = mean
+)
 names(preco.papel.agregado)[2] <- "Volume Médio"
 tickers.negociados <- tickers
 tickers.negociados <- left_join(tickers.negociados,
-                                preco.papel.agregado,
-                                by=c("simbolo" = "Tickers"))
+  preco.papel.agregado,
+  by = c("simbolo" = "Tickers")
+)
 tickers.negociados <- tickers.negociados %>%
   group_by(CD_CVM) %>%
   top_n(1, `Volume Médio`)
 
 saveRDS(tickers.negociados, "Data/tickers_mais_negociados")
 
+# tratamento de memória
 rm(list = ls())
 gc()
-
