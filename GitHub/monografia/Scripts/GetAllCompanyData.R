73ccab750ab0ff6dad4e3fc958e79dc737fc40fa
camurca1 <alexandre.camurca@unesp.br>
2021-08-10 17:49:33 -0300

    Melhorias de performance e documentação.

--- GitHub/monografia/Scripts/GetAllCompanyData.R
+++ GitHub/monografia/Scripts/GetAllCompanyData.R
@@ -1,14 +1,31 @@
+# Escrito por: Alexandre Camurça Silva de Souza
+# Ambiente RStudio Desktop 1.4.1717 "Juliet Rose"
+
+# Etapa 1 - Download das informações das empresas listadas
+
+
+# limpar memória e desativar notação científica
 rm(list = ls())
 options(scipen = 999)
 
+#### Gereciamento de pacotes ####
 
-library(GetDFPData2)
-library(GetDFPData)
-library(GetFREData)
-library(tidyverse)
-library(tidyr)
-library(lubridate)
+# informar os pacotes que serao utilizados no script
+pacotes <- c("GetDFPData2", "GetDFPData", "GetFREData",
+             "tidyverse", "tidyr", "lubridate")
 
+# instalar pacotes ausentes
+pacotes_instalados <- pacotes %in% rownames(installed.packages())
+if (any(pacotes_instalados == FALSE)){
+  install.packages(c(pacotes[!pacotes_instalados]))
+}
+
+# carregar pacotes
+invisible(lapply(pacotes, library, character.only = TRUE))
+
+#### Download dos dados ####
+
+# baixar, salvar e tratar os dados cadastrais das empresas
 cia.info <- tibble(get_info_companies(tempdir()))
 cia.info2 <- tibble(gdfpd.get.info.companies(type.data = 'companies',
                                              cache.folder = tempdir()))
@@ -41,7 +58,7 @@ cia.info2_reduzido <- cia.info2_reduzido %>%
   filter(codigos.cvm != 12653)
 saveRDS(cia.info2_reduzido, file = "Data/cia_info_reduzido")
 
-
+# salvar os códigos de negociação
 tickers <- as.data.frame(cia.info2_reduzido$codigos.cvm)
 names(tickers)[1] <- "CD_CVM"
 tickers$symbols <- cia.info2_reduzido$tickers
@@ -59,6 +76,7 @@ saveRDS(tickers, "Data/tabelatickers")
 
 codigos.cvm <- c(t(cia.info2_reduzido$codigos.cvm))
 
+# baixar e salvar os demonstrativos financeiros padronizados
 l.dfp <- get_dfp_data(companies_cvm_codes = codigos.cvm,
                       use_memoise = FALSE,
                       clean_data = TRUE,
@@ -68,13 +86,13 @@ l.dfp <- get_dfp_data(companies_cvm_codes = codigos.cvm,
                       last_year = 2020)
 saveRDS(l.dfp, file = "Data/cia_dfp")
 
-
+# baixar e salvar os formulários cadastrais
 l.fre <- get_fre_data(companies_cvm_codes = codigos.cvm,
                       fre_to_read = 'last',
                       first_year = 2015,
                       last_year = 2021)
 saveRDS(l.fre, file = "Data/cia_fre")
 
-
+# tratamento de memória
 rm(list = ls())
 gc()
