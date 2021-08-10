68e1ee6dc40b7f7664c4154869396099fca3c2ff
camurca1 <alexandre.camurca@unesp.br>
2021-08-10 17:52:57 -0300

    Melhorias de performance e documentação.

--- GitHub/monografia/Scripts/CalculateModernTheory.R
+++ GitHub/monografia/Scripts/CalculateModernTheory.R
@@ -1,10 +1,29 @@
+# Escrito por: Alexandre Camurça Silva de Souza
+# Ambiente RStudio Desktop 1.4.1717 "Juliet Rose"
+
+# Etapa 7 - Cálculo da fronteira e da carteira eficiente
+
+
+# limpar memória e desativar notação científica
 rm(list = ls())
 options(scipen = 999)
 
-library(tidyverse)
-library(fPortfolio)
+#### Gereciamento de pacotes ####
+
+# informar os pacotes que serao utilizados no script
+pacotes <- c("tidyverse", "fPortfolio")
+
+# instalar pacotes ausentes
+pacotes_instalados <- pacotes %in% rownames(installed.packages())
+if (any(pacotes_instalados == FALSE)) {
+  install.packages(c(pacotes[!pacotes_instalados]))
+}
+
+# carregar pacotes
+invisible(lapply(pacotes, library, character.only = TRUE))
 
 
+#### carregar tabelas salvas ####
 ret.vol.acoes <- readRDS("Data/retorno_volatilidade_acoes")
 
 precos.acoes <- as.data.frame(ret.vol.acoes$CD_CVM)
@@ -13,6 +32,7 @@ precos.acoes$ref.date <- ret.vol.acoes$ref.date
 precos.acoes$retornos <- ret.vol.acoes$retornos
 precos.acoes <- subset(precos.acoes, precos.acoes$ref.date < "2016-01-01")
 
+#### construir a matriz de retornos ####
 retornos.matriz <- precos.acoes
 names(retornos.matriz)[1] <- "CD_CVM"
 retornos.matriz$CD_CVM <- NULL
@@ -26,16 +46,22 @@ retornos.matriz <- retornos.matriz %>%
 retornos.matriz$ref.date <- NULL
 retornos.matriz <- as.timeSeries(retornos.matriz)
 
+
+#### definir parâmetros do cálculo da fronteira ####
 specs <- portfolioSpec()
 `setNFrontierPoints<-`(specs, 1000)
 
-portfolio.eficiente <- tangencyPortfolio(retornos.matriz, spec = specs, constraints = "LongOnly")
+
+#### calcular fronteira eficiente e gerar gráficos ####
+portfolio.eficiente <- tangencyPortfolio(retornos.matriz, spec = specs,
+                                         constraints = "LongOnly")
 weightsPie(portfolio.eficiente, col = qualiPalette(n = 76, "Set1"))
 mtext(text = "Pesos - Portifolio Eficiente", side = 3, line = 1.5,
       font = 2, cex = 0.7, adj = 0)
 
 
-fronteira <- portfolioFrontier(retornos.matriz, spec = specs, constraints = "LongOnly")
+fronteira <- portfolioFrontier(retornos.matriz, spec = specs,
+                               constraints = "LongOnly")
 
 
 frontierPlot(fronteira, col = c('blue', 'red'), pch = 20, frontier = "upper",
@@ -54,9 +80,12 @@ singleAssetPoints(fronteira, return = "mean", risk = "Cov",
                        auto = FALSE, cex = 1.5,
                        col = rainbow(6), lwd = 2,
                   ylim = c(0, 0.002),
-                  text(xy[, 1], xy[, 2], pos = 3, rownames(xy), font = 2, cex = 0.7))
+                  text(xy[, 1], xy[, 2], pos = 3,
+                       rownames(xy), font = 2, cex = 0.7))
 
 dados.portifolio <- getPortfolio(portfolio.eficiente)
+
+#### extrair carteira eficiente ####
 carteira <- as.data.frame(dados.portifolio$weights)
 carteira$Tickers <- row.names(carteira)
 row.names(carteira) <- NULL
@@ -67,3 +96,7 @@ carteira <- carteira %>% select(Tickers, Pesos)
 saveRDS(dados.portifolio, "Data/dados_carteira_eficiente")
 saveRDS(carteira, "Data/carteira_eficiente")
 write.csv2(carteira, "Data/carteira_eficiente.csv", row.names = F)
+
+# limpeza de memória
+rm(list = ls())
+gc()
