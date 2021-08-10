2fbc0ba2ed56daad979c7b8a9ddbd103f81a2d0c
camurca1 <alexandre.camurca@unesp.br>
2021-08-10 17:52:33 -0300

    Melhorias de performance e documentação.

--- GitHub/monografia/Scripts/CalculateTOPSISMCMD.R
+++ GitHub/monografia/Scripts/CalculateTOPSISMCMD.R
@@ -1,23 +1,41 @@
+# Escrito por: Alexandre Camurça Silva de Souza
+# Ambiente RStudio Desktop 1.4.1717 "Juliet Rose"
+
+# Etapa 6 - Elaboração do Ranking TOPSIS
+
+
+# limpar memória e desativar notação científica
 rm(list = ls())
 options(scipen = 999)
-set.seed(10)
+
+#### Gereciamento de pacotes ####
+
+# informar os pacotes que serao utilizados no script
+pacotes <- c("topsis", "bizdays", "dplyr", "lubridate")
+
+# instalar pacotes ausentes
+pacotes_instalados <- pacotes %in% rownames(installed.packages())
+if (any(pacotes_instalados == FALSE)) {
+  install.packages(c(pacotes[!pacotes_instalados]))
+}
+
+# carregar pacotes
+invisible(lapply(pacotes, library, character.only = TRUE))
 
 
-library(topsis)
-library(bizdays)
-library(dplyr)
-library(lubridate)
-
+#### criar calendário de dias úteis ####
 holidaysANBIMA <- append(holidaysANBIMA,  ymd(c("2016-12-30", "2017-12-29")))
 cal <- create.calendar("Brazil/ANBIMA",
                        holidaysANBIMA, weekdays=c("saturday", "sunday"))
 
+
+#### carregar tabelas salvas ####
 ind.fundamento <- (readRDS("Data/indices_calculados"))
 ind.tecnico <- (readRDS("Data/indAT"))
 bp <- (readRDS("Data/balanco_patrimonial"))
 
 
-
+#### criar matriz de decisão ####
 amd.matriz <- subset(ind.fundamento,
                      ind.fundamento$DT_REFER == "2015-12-31")
 amd.matriz$dia.util.ant <- add.bizdays(amd.matriz$DT_REFER, -1, cal)
@@ -36,9 +54,12 @@ amd.matriz <- as.matrix(amd.matriz)
 rm(ind.fundamento, ind.tecnico)
 gc()
 
+#### definir parametros dos critérios ####
 pesos <- c(rep(1/ncol(amd.matriz), ncol(amd.matriz)))
 critPosNeg <- c("+", "-", "+", "+", "+", "+", "+")
 
+
+#### construir ranking ####
 ranking <- topsis(amd.matriz, pesos, critPosNeg)
 ranking$ticker <- row.names(amd.matriz)
 ranking$alt.row <- NULL
@@ -50,6 +71,7 @@ top.30 <- slice_max(ranking, order_by = ranking$score, n = 30)
 rm(bp, amd.matriz)
 gc()
 
+#### extrair carteiras do ranking ####
 carteira1 <- slice_max(ranking, order_by = ranking$score, n = 15)
 carteira1$pesos <- 1/nrow(carteira1)
 carteira2 <- slice_min(ranking, order_by = ranking$score, n = 15)
@@ -66,8 +88,9 @@ l.carteiras <- list("carteira1" = carteira1,
 saveRDS(l.carteiras, "Data/carteirasTOPSIS")
 saveRDS(ranking, "Data/rankingTOPSIS")
 
-write.csv2(l.carteiras, "Data/carteirasTOPSIS.csv", row.names = F)
-write.csv2(ranking, "Data/ranking_ativos.csv", row.names = F)
+# write.csv2(l.carteiras, "Data/carteirasTOPSIS.csv", row.names = F)
+# write.csv2(ranking, "Data/ranking_ativos.csv", row.names = F)
 
+# limpeza de memória
 rm(list = ls())
 gc()
